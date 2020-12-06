1f87bc2f38e11af80b8c42f544fe3ea5e16f0022
Stalin Chriss Castelino <73968355+stalincc@users.noreply.github.com>
2020-12-06 23:45:37 +0100

    Update Circle.java

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -2,17 +2,17 @@ public class Circle extends GeometricObject {
 
 	double radius;
   
- 	public Circle(){
+    public Circle(){
 	
-	}
+    }
 	
-	public double getArea(){
+    public double getArea(){
 		
   	return(Math.PI * (radius*radius));
 		
-  	}
+    }
 	
-  public double getPerimeter(){
+    public double getPerimeter(){
     
 	  return(Math.PI*2*radius);
 	  
@@ -29,12 +29,12 @@ public class Circle extends GeometricObject {
 	  
   }
 
-    public void printCircle(){
+   public void printCircle(){
       
            System.out.println("The circle is created" + getDateCreated() + " and the radius is " + radius);
     }
 		
-    public String toString() {
+   public String toString() {
 		
 	    return ("Circle created on " + getDateCreated() + "\ncolor: " + 
         	     this.getColor() + "and filled " + this.isFilled() + " and radius: " + radius);
