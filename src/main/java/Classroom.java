f5fbd2b3837a707d36a47efc528944dd1d0edcc8
Stalin Chriss Castelino <73968355+stalincc@users.noreply.github.com>
2020-12-06 17:55:43 +0100

    Update Classroom.java

--- src/main/java/Classroom.java
+++ src/main/java/Classroom.java
@@ -57,31 +57,72 @@ public class Classroom{
   
    public String getTerm(){
   
-    return this.term;
+      Calendar cal = new GregorianCalendar();
+        int m = cal.get(Calendar.MONTH);
+        int y = cal.get(Calendar.YEAR);
+
+        if(m> 2 && m < 10)
+        {
+            this.term = "SS" + y;
+            return this.term;
+        }
+        else
+        {
+            this.term = "WS" + y + "/" + ((y%2000)+1);
+            return this.term;
+        }
     
   }
   
   public String getID(){
   
-
+    id = this.course.getID();
+    return (id + "-" + this.term);
     
   }
   
   public void addStudent(Student studentarg){
+    
+    List<Student> tempList = new ArrayList<Student>(Arrays.asList(participants)); 
+  
+    tempList.add(student); 
+  
+       participants = tempList.toArray(participants); 
   
     
     
   }
   
-  public String getParticipants(){
+  public String getParticipantsEmail(){
   
+    String str= "";
+           
+        
+        for(int i=0; i<participants.length; i++){
+            if(i == (participants.length-1)){
+                str += (participants[i].getID() + "@student.hsrw");
+            }
 
+            else if (i == (participants.length-1)){
+
+            str += "";
+
+        }
+                                         
+          else{
+
+          str +=( participants[i].getID() + "@student.hsrw, ");
+
+          }
+               
+        }
+        return str;
     
   }
   
   public String getTotalParticipants(){
   
-    
+    return this.participants.length;
 
     
   }
