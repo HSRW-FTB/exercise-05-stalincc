e22f2f0e1c7dc40169cbd45423c4f9cbf4f28fdf
Stalin Chriss Castelino <73968355+stalincc@users.noreply.github.com>
2020-12-07 00:20:30 +0100

    Update BucketOfObjects.java

--- src/main/java/BucketOfObjects.java
+++ src/main/java/BucketOfObjects.java
@@ -1,4 +1,4 @@
-/*import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class BucketOfObjects{
 
@@ -56,36 +56,4 @@ public class BucketOfObjects{
   
   
 
-} */
-
-import java.util.ArrayList;
-public class BucketOfObjects {
-    private ArrayList<GeometricObject> items = new ArrayList<GeometricObject>(); 
-    
-    public BucketOfObjects(){
-    }
-    public void addObject(GeometricObject g){
-        items.add(g);
-    }
-    public double getTotalArea(){
-        double totArea = 0;
-        for (GeometricObject geo : items){
-            totArea += geo.getArea();
-        }
-        return totArea; 
-    }
-    public double getTotalPerimeter(){
-        double totPeri = 0;
-        for (GeometricObject geo : items){
-            totPeri += geo.getPerimeter();
-        }
-        return totPeri;
-    }
-    public String toString(){
-        String str = ""; 
-        for (GeometricObject geo: items ){
-            str += geo.getClass().getSimpleName()+", "; 
-        }
-        return str = str.substring(0,str.length()-2);
-    }
 }
