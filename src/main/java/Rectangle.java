cb1718ab05bb3a9ae26ef00beccd2c3c7e5c16dc
Stalin Chriss Castelino <73968355+stalincc@users.noreply.github.com>
2020-12-06 23:50:21 +0100

    Update Rectangle.java

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,26 +1,48 @@
 public class Rectangle extends GeometricObject{
     
-    double height=1;
-    double width=1;
+   double height=1;
+   double width=1;
   
-   Rectangle() { }
+   Rectangle(){
+   
+   }
     
    Rectangle(double newHeight,double newWidth) {
         height = newHeight;
         width = newWidth; 
    }
     
+    Rectangle(double newWidth, double newHeight, String color, boolean filled) {
+        super(color, filled); 
+        height = newHeight;
+        width = newWidth;
+    } 
+    
   public double getArea(){
+      
     return width*height;
+      
    }    
        
    public double getPerimeter(){
+       
     return (2*(width+height));
-                        } 
-public void changeColor(String color){
-    super.setColor(color);
+       
+    } 
+    
+    public void changeColor(String color){
+        
+        super.setColor(color);
 
-}
+    }
+    
+    public void setFilled(int n) {
+        
+        if (n > 0){
+            super.setFilled(true);}
+        else{
+            super.setFilled(false);}
+    }
 
 
 }
